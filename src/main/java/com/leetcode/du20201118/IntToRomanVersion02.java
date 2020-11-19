14e67a2e878fa0ae9cbd3fd018765f0e85c36a4d
du_wenlei <823633682@qq.com>
2020-11-19 11:47:05 +0800

    整数转罗马数字 贪心算法方式实现

---
+++ src/main/java/com/leetcode/du20201118/IntToRomanVersion02.java
@@ -0,0 +1,29 @@
+package com.leetcode.du20201118;
+
+/**
+ * @author duwenlei
+ **/
+public class IntToRomanVersion02 {
+    /**
+     * 贪心算法实现
+     *
+     * @param num 整数 1-3999
+     * @return
+     */
+    public static String intToRoman(int num) {
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] lm = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            while (num >= nums[i]) {
+                num = num - nums[i];
+                sb.append(lm[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(5));
+    }
+}
