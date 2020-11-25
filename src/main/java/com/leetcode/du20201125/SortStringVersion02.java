218ed9b503e5be97f5e06f22cd62ae262c051e86
du_wenlei <823633682@qq.com>
2020-11-25 14:04:30 +0800

    上升下降字符串

---
+++ src/main/java/com/leetcode/du20201125/SortStringVersion02.java
@@ -0,0 +1,25 @@
+package com.leetcode.du20201125;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author duwenlei
+ **/
+public class SortStringVersion02 {
+    /**
+     * 暴力处理
+     *
+     * @param s
+     * @return
+     */
+    public static String sortString(String s) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortString("leetcode"));
+    }
+}
