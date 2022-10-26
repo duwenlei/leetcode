7caa3dfd6534093428c0580858e4a6b0d25c31d2
duwenlei <952589031@qq.com>
2022-10-26 10:27:25 +0800

    20221026 N皇后问题

---
+++ src/main/java/com/leetcode/y2022/m10/d26/TestSolveNQueens.java
@@ -0,0 +1,16 @@
+package com.leetcode.y2022.m10.d26;
+
+import java.util.List;
+
+/**
+ * @author duwenlei
+ * @version 1.0
+ * @className TestSolveNQueens
+ * @date 2022/10/26 10:26
+ */
+public class TestSolveNQueens {
+    public List<List<String>> solveNQueens(int n) {
+
+        return null;
+    }
+}
